216d16e6e3c2d7b2a357872d14bb6bd1f0650862
user <user@eecs-vbox-common.cs.yorku.ca>
2015-05-12 11:19:54 -0400

    Controller commented

--- Checker/src/client/Controler.java
+++ Checker/src/client/Controler.java
@@ -23,7 +23,10 @@ import model.InputValidator;
 import model.WSClient;
 
 /**
- * Servlet implementation class Controler
+ * The main controller of the web accessibility checker
+ * The main controller handles all client requests, delegates to the model to handle business logic and finally 
+ * responds to the client. 
+ * 
  */
 @WebServlet("/Controler")
 public class Controler extends HttpServlet {
@@ -32,66 +35,96 @@ public class Controler extends HttpServlet {
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public Controler() {
+    public Controler() 
+    {
         super();        
     }
-    
+    /**
+     * Initialize the server, instantiate all model and xml components to optimize server performance during runtime
+     */
     public void init() throws ServletException
     {
         this.getServletContext().setAttribute("crawler", new Crawler());
         this.getServletContext().setAttribute("wsclient", new WSClient());
         this.getServletContext().setAttribute("xmlCreator", new XMLCreator());
+        this.getServletContext().setAttribute("validator", new InputValidator());
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		InputValidator validate = new InputValidator();
+		InputValidator validate = (InputValidator) this.getServletContext().getAttribute("validator");
 		
+		/*
+		 * List of target JSP pages the controller may send clients to
+		 */
 		String jspTarget = "";
 		String initialPage = "/start.jspx";
 		String resultsPage = "/Results.jspx";
 		
+		/*
+		 * Examine the parameters in the request, if submit is set, the user must have pressed the submit button; validate their inputs
+		 * If no submit then assume fresh visit and send user to the landing page
+		 */
 		if(request.getParameter("submit") == null)
 		{
 			jspTarget = initialPage;
 		}else
 		{
 			try{
+				
+				/*
+				 * Extract parameters expected from the form
+				 */
 				String url = request.getParameter("url");
 				String depth = request.getParameter("depth");
 				String isPrefix = request.getParameter("isPrefix");
 				
-				
+				/*
+				 * Set extracted parameters into the request scope to allow for "back" and "redo" functionality
+				 */
 				request.setAttribute("url", url);
 				request.setAttribute("depth", depth);
 				request.setAttribute("isPrefix", isPrefix);
 
+				/*
+				 * Validate the URL and depth supplied by the user
+				 */
 				validate.validateURL(url);
 				validate.validateDepth(depth);
 				
-				//Inputs validated
-
+				/*
+				 * Input validation has succeeded, cast parameters to their correct types for processing by the model
+				 */
 				int d = Integer.parseInt(depth);
 				boolean p = (request.getParameter("isPrefix") == null) ? false : true;
 
 				Crawler c = (Crawler) this.getServletContext().getAttribute("crawler");
 				WSClient ws = (WSClient) this.getServletContext().getAttribute("wsclient");
 				
-				//Get the links to evaluate
+				/*
+				 * Pass parameters to the web crawler to retrieve the set of links to evaluate
+				 */
 				String[] toEvaluate = c.retrieveLinks(url, p, d);
 				
-				//Evaluate Links
+				/*
+				 * For each webpage found; evaluate the webpage and place it into the results. 
+				 * Results are formatted into a map where keys are a unique integer ID and values and an Issue object
+				 */
 				Map<Integer,Issue> issues = ws.getIssues(toEvaluate);
 
-				//Convert the map to XML for download if required
+				/*
+				 * Wrap the map of result for xml marshalling as well as results.jspx printing
+				 * In addition to wrapping, calculate the statistics of the errors found (Number of each type of issue) 
+				 */
 				errorWrapper ew = new errorWrapper();
 				ew.setMap(issues);
 				ew.calcStats();
 				
-				//Get XML File attributes.
+				/*
+				 * Pass error wrapper object to model for JAXB marshalling to xml
+				 */
 				String xmlFileFolder = "/Results";
 				String xmlFileLocation = this.getServletContext().getRealPath(xmlFileFolder); 
 				String fileName = request.getSession().getId() + ".xml";
@@ -102,12 +135,14 @@ public class Controler extends HttpServlet {
 				XMLCreator xc = (XMLCreator) this.getServletContext().getAttribute("xmlCreator");
 				xc.createXML(location, ew);
 				
-				//System.out.println("File Location: " + xmlFileLocation);
-				//System.out.println("File Name " + fileName);
-				//System.out.println("Url: " + url + " depth: " + depth + " isPrefix: " + isPrefix);
-				
+				/*
+				 * Add the error wrapper to the request scope to allow results.jspx to parse it
+				 */
 				request.setAttribute("results", ew);
 				
+				/*
+				 * Set the results.jspx page as the page to forward the user to
+				 */
 				jspTarget = resultsPage;
 			}catch(MalformedURLException e)
 			{
@@ -139,7 +174,9 @@ public class Controler extends HttpServlet {
 			}
 
 		}
-		
+		/*
+		 * Forward user to the set page
+		 */
 		request.getRequestDispatcher(jspTarget).forward(request,response);
 	}
 
@@ -147,7 +184,7 @@ public class Controler extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		doGet(request,response);
 	}
 
 }
